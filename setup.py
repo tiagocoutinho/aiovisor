603cb23ec63818e082e37c60674d5af9a008fb41
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-06-08 18:11:50 +0200

    Prepare setup

--- setup.py
+++ setup.py
@@ -2,7 +2,15 @@
 
 """The setup script."""
 
+import sys
 from setuptools import setup, find_packages
+
+TESTING = any(x in sys.argv for x in ["test", "pytest"])
+
+setup_requirements = []
+if TESTING:
+    setup_requirements += ['pytest-runner']
+test_requirements = ['pytest', 'pytest-cov']
 
 with open("README.md") as f:
     description = f.read()
@@ -19,5 +27,8 @@ setup(
     packages=find_packages(),
     url="https://gitlab.com/tiagocoutinho/aiovisor",
     version="0.1.0",
-    python_requires=">=3.7"
+    python_requires=">=3.7",
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements
 )
